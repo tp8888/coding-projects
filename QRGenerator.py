fa23c96f95708ca5227c4a1ed08211951f284635
tp8888 <69043644+tp8888@users.noreply.github.com>
2023-11-12 09:11:16 -0500

    Add files via upload

--- QRGenerator.py
+++ QRGenerator.py
@@ -1,46 +1,49 @@
 import qrcode
 from tkinter import *
 
+# title, size
 cp=Tk()
 cp.title('QR Generator')
-cp.geometry('700x250')
-cp.config(bg='#e52165')
+cp.geometry('500x500')
+cp.config(bg='#261797')
+
+# file saved! message
 
 def generate():
   img = qrcode.make(msg.get())
   type(img)
   img.save(f'{save_name.get()}.png')
-  Label(cp, text='File Saved!', bg='#e52165', fg='black', font=('Arial Black', 8)).pack()
+  Label(cp, text='File Saved!', bg='#3353fa', fg='black', font=('Arial Black', 18)).pack()
 
 def show():
   img = qrcode.make(msg.get())    
   type(img)
   img.show()
 
-frame = Frame(cp,bg='#e52165')
+frame = Frame(cp,bg='#3353fa')
 frame.pack(expand=True)
 
 # enter the text or url
 
-Label (frame,text='Enter the Text or URL:',font=('Arial Black',16),
-   bg='#e52165').grid(row=0,column=0,sticky='w')
+Label (frame,text='Enter Text or URL:',font=('Arial Black',14),
+   bg='#3353fa').grid(row=0,column=0,sticky='w')
 
 msg = Entry(frame)
 msg.grid(row=0,column=1)
 
 # enter the file name
 
-Label( frame,text='File Name(Save As):',font=('Arial Black',16),
-   bg='#e52165').grid(row=1, column=0, sticky='w')
+Label( frame,text='File Name(Save As):',font=('Arial Black',14),
+   bg='#3353fa').grid(row=1, column=0, sticky='w')
 
 save_name = Entry(frame)
 save_name.grid(row=1, column=1)
 
 # buttons to show or save qrcode
 
-btn = Button(cp, text='Show QR code', bd='5', command=show, width=15)
+btn = Button(cp, text='Show QR code', bd='15', command=show, width=15)
 btn.pack()
-btn = Button(cp, text='Save QR code', command=generate, bd='5',width=15)
+btn = Button(cp, text='Save QR code', command=generate, bd='15',width=10)
 btn.pack()
 
 cp.mainloop()
